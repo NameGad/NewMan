8ce75f2c054f3bfd6e3baf6fa408722eddac2031
0+* <1525613414@qq.com>
2022-08-06 19:49:33 +0800

    Vue2完结最后总提交

---
+++ font-endStudy/Vue/19_单文件组件/School.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>校名：{{name}}</h1>
+    <h2>地区：{{address}}</h2>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+    import Student from './Student.vue';
+    export default {
+        data() {
+            return {
+                name:'SZIIT',
+                address: '深圳'
+            }
+        },
+        components:{
+            Student
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ font-endStudy/Vue/19_单文件组件/Student.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>姓名：{{name}}</h1>
+    <h2>年龄：{{age}}</h2>
+  </div>
+</template>
+
+<script>
+    export default {
+      name:'Student',
+        data() {
+            return {
+                name:'林佳成',
+                age: '22'
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
