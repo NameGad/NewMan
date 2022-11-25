7ab4c9fccffc26ffeec954c41ece25927fbc4cd5
0+* <1525613414@qq.com>
2022-11-25 20:07:27 +0800

    一些修改提交

--- font-endStudy/Webpack4&5/webpack_4/webpack4_TianYu/src/js/modules/module2.js
+++ font-endStudy/Webpack4&5/webpack_4/webpack4_TianYu/src/js/modules/module2.js
@@ -1,7 +1,16 @@
-// 统一暴露
+// 分别暴露
 export function add(a, b) {
   console.log(a + b);
 }
 export function sub(a, b) {
   console.log(a - b);
 }
+
+// 2. 统一暴露, m2.js
+let school = 'gc';
+
+function findJob() {
+    console.log("m2---我们可以帮助你找工作!!");
+};
+
+export {school, findJob};
